8a36422db65acbdf2cfb6d3ab0085f21c6d86d1a
WontonsofDMG <mitchelltai221@gmail.com>
2025-04-14 16:40:49 -0700

    Added the config lua with window size and title edits.

---
+++ love2dProjects/conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.width = 1000 
+    t.window.height = 300
+    t.window.resizable = false -- Makes it so people can't f*k with the window
+    t.window.title = "Skate-170🛹 Totally Rad Map Viewer" -- Feel free to change this
+end
